6b5083b529528656317bbee2e8be0dcf93693c4a
Vignesh Nethaji <vigneshnethajichengam@gmail.com>
2024-02-24 00:28:47 +0530

    Initial commit

---
+++ BloodBank.Management.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BloodBank.Management.Web.MvcApplication" Language="C#" %>
---
+++ BloodBank.Management.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BloodBank.Management.Web.Startup))]
+namespace BloodBank.Management.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
